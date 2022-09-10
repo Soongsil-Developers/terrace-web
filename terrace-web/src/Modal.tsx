88eea4b4c873bec82cc5e0e286ca75ec2d70e78d
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-09-10 23:49:40 +0900

    [추가] Modal 구현 마무리 #22

--- terrace-web/src/Modal.tsx
+++ terrace-web/src/Modal.tsx
@@ -1,47 +1,81 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Modal.css";
 
 export default function ReserveModal(props: any) {
-  const [show, setShow] = useState(false);
+  const [showPossible, setShowPossible] = useState(false);
+  const [showImpossible, setShowImpossible] = useState(false);
+  const [buttonChange, setButtonChange] = useState(false);
+  const [message, setMessage] = useState("room" + props.roomNumber);
 
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  // const handleClose = () => setShowPossible(false);
+  const handleShowPossible = () => setShowPossible(!showPossible);
+  const handleShowImpossible = () => setShowImpossible(!showImpossible);
   const handleReserve = () => {
-    setShow(false);
+    setShowPossible(!showPossible);
+    setMessage("사용중");
+    setButtonChange(!buttonChange);
   };
 
   return (
     <>
-      <Button id="openReserveModal" onClick={handleShow}>
-        room{props.roomNumber}
+      <Button
+        id={"openModal" + (buttonChange ? "Impossible" : "Possible")}
+        onClick={buttonChange ? handleShowImpossible : handleShowPossible}
+      >
+        <p className="btnMessage">{message}</p>
       </Button>
 
       <Modal
-        show={show}
-        onHide={handleClose}
+        show={showPossible}
+        onHide={handleShowPossible}
         aria-labelledby="contained-modal-title-vcenter"
         centered
       >
         <Modal.Header>
           <Modal.Title>
-            <p id="modalTitle">테라스 선택 확인</p>
+            <p className="modalTitle">테라스 선택 확인</p>
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <span id="modalBody">
+          <span className="modalBody">
             테라스 room{props.roomNumber}을 <br />
             예약하시겠습니까?
           </span>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button
+            id="cancelBtn"
+            variant="secondary"
+            onClick={handleShowPossible}
+          >
             취소
           </Button>
-          <Button id="reserveBtn" onClick={handleClose}>
+          <Button id="reserveBtn" onClick={handleReserve}>
             예약
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <Modal
+        show={showImpossible}
+        onHide={handleShowPossible}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header>
+          <Modal.Title>
+            <p className="modalTitle">테라스 선택 확인</p>
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <span className="modalBody">현재 사용중입니다.</span>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button id="okBtn" onClick={handleShowImpossible}>
+            OK
           </Button>
         </Modal.Footer>
       </Modal>
