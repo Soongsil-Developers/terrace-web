0a336d7fed83cf4bc6f508b0d5aeac8dd34eb625
sihyuuun <leesh3597@gmail.com>
2022-09-12 04:46:19 +0900

    [추가] qr scanner 생성 #19

---
+++ terrace-web/src/Scanner.tsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { QrReader } from "react-qr-reader";
+import BarcodeScannerComponent from "react-qr-barcode-scanner";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function Scanner() {
+  const [scan, setScan] = useState(true);
+  const [logs, setLog] = useState<Array<string>>([]);
+  const qrReaderHandleScan = (data: any) => {
+    if (data) {
+      setLog([...logs, data]);
+      setScan(false);
+    }
+  };
+
+  const qrReaderHandleError = (error: any) => {
+    setLog([...logs, error.message]);
+  };
+
+  const barcodeScannerComponentHandleUpdate = (error: any, result: any) => {
+    if (result) {
+      setLog([...logs, result.text]);
+      setScan(false);
+    }
+  };
+
+  const previewStyle = {
+    height: 500,
+    width: 400,
+  };
+  return (
+    <div className='app'>
+      <div>
+        <QrReader
+          style={previewStyle}
+          onScan={qrReaderHandleScan}
+          onError={qrReaderHandleError}
+        />
+        <BarcodeScannerComponent
+          onUpdate={barcodeScannerComponentHandleUpdate}
+        />
+      </div>
+      {/* <div>
+        {logs.map((log) => (
+          <div key={log}>{log}</div>
+        ))}
+        <button onClick={() => setLog([])}>CLEAR</button>
+      </div> */}
+    </div>
+  );
+}
