bd48be5912860fbb569ded0c26bdb937ec9e5c8d
Yujin <jini1514@soongsil.ac.kr>
2022-09-13 16:13:13 +0900

    [제거] MyPage 삭제 및 경로 변경
    이전) 메인->메인페이지->헤더->마이페이지버튼->네비게이션
    이후) 메인->헤더->네비게이션

--- src/Header.tsx
+++ src/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import "./Header.css";
 import { FaUser } from "react-icons/fa";
 import MyPageBtn from "./MyPage";
+import Navigation from "./Navigation";
 
 const Header = () => (
   <div id="header">
     <div id="headerContents">
       <text id="headerText">테라스 예약 시스템</text>
-      <MyPageBtn />
-      {/* <Navigation />  */}
+      {/* <MyPageBtn /> */}
+      <Navigation /> 
     </div>
   </div>
 );
