19337912c5e37c7694e40eae34278f55e0b1dd98
SeonHwan-Kim <rlatjsghks4647@naver.com>
2022-09-17 02:44:56 +0900

    [추가] loginpage 추가

---
+++ src/LoginPage.tsx
@@ -0,0 +1,35 @@
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
+import { FaUser, FaLock } from "react-icons/fa";
+// import Link from "react-router-dom";
+import "./LoginPage.css";
+
+const LoginPage = () => {
+  return (
+    <Form>
+      <div id="loginPage">
+        <div>
+          <p id="loginTitle">로그인</p>
+        </div>
+        <div id="inputLoginInfo">
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <FaUser size="25" color="#A4A4A4" />
+            <Form.Control type="email" placeholder="U-Saint ID" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <FaLock size="25" color="#A4A4A4" />
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <div id="loginBtnDiv">
+            <Button id="loginBtn" variant="primary" type="submit">
+              <p id="loginBtnText">로그인</p>
+            </Button>
+          </div>
+        </div>
+      </div>
+    </Form>
+  );
+};
+
+export default LoginPage;
